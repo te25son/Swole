d2caafb5605ab42406c87353512d072885384e27
te25son <te25son@gmail.com>
2021-03-01 09:18:19 +0100

    Add gym data loader

--- Swole/Queries/GymQueries.cs
+++ Swole/Queries/GymQueries.cs
@@ -2,8 +2,11 @@
 using HotChocolate.Types;
 using Microsoft.EntityFrameworkCore;
 using Swole.Data;
+using Swole.DataLoaders;
 using Swole.Extensions;
+using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Swole.Queries
@@ -14,5 +17,8 @@ namespace Swole.Queries
         [UseAppDbContext]
         public Task<List<Gym>> GetGyms([ScopedService] AppDbContext context) =>
             context.Gyms.ToListAsync();
+
+        public Task<Gym> GetGymAsync(Guid id, GymByIdDataLoader dataLoader, CancellationToken cancellationToken) =>
+            dataLoader.LoadAsync(id, cancellationToken);
     }
 }
--- Swole/Startup.cs
+++ Swole/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Swole.Data;
+using Swole.DataLoaders;
 using Swole.Mutations;
 using Swole.Queries;
 
@@ -19,10 +20,14 @@ namespace Swole
 
             services
                 .AddGraphQLServer()
+
                 .AddQueryType(d => d.Name("Query"))
                     .AddTypeExtension<GymQueries>()
+
                 .AddMutationType(d => d.Name("Mutation"))
-                    .AddTypeExtension<GymMutations>();
+                    .AddTypeExtension<GymMutations>()
+
+                .AddDataLoader<GymByIdDataLoader>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
