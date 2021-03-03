0a66eacd76c2200b5070b736163d414d19c0bbfb
te25son <te25son@gmail.com>
2021-03-03 09:03:15 +0100

    Add member data loader

---
+++ Swole/DataLoaders/MemberByIdDataLoader.cs
@@ -0,0 +1,14 @@
+﻿using GreenDonut;
+using Microsoft.EntityFrameworkCore;
+using Swole.Data;
+
+namespace Swole.DataLoaders
+{
+    public class MemberByIdDataLoader : IdDataLoaderBase<Member>
+    {
+        public MemberByIdDataLoader(IBatchScheduler batchScheduler, IDbContextFactory<AppDbContext> dbContextFactory)
+            : base(batchScheduler, dbContextFactory)
+        {
+        }
+    }
+}
--- Swole/Types/GymType.cs
+++ Swole/Types/GymType.cs
@@ -20,6 +20,12 @@ namespace Swole.Types
                 .ResolveWith<GymResolvers>(i => i.GetEmployeesAsync(default!, default!, default!, default))
                 .UseDbContext<AppDbContext>()
                 .Name("employees");
+
+            descriptor
+                .Field(g => g.Members)
+                .ResolveWith<GymResolvers>(i => i.GetEmployeesAsync(default!, default!, default!, default))
+                .UseDbContext<AppDbContext>()
+                .Name("members");
         }
 
         private class GymResolvers
@@ -37,6 +43,21 @@ namespace Swole.Types
                     .ToArrayAsync();
 
                 return await employeeById.LoadAsync(employeeIds, cancellationToken);
+            }
+
+            public async Task<IEnumerable<Member>> GetMembersAsync(
+                Gym gym,
+                [ScopedService] AppDbContext context,
+                MemberByIdDataLoader memberById,
+                CancellationToken cancellationToken)
+            {
+                var memberIds = await context.Gyms
+                    .Where(g => g.Id == gym.Id)
+                    .Include(g => g.Members)
+                    .SelectMany(g => g.Members.Select(e => e.GymId))
+                    .ToArrayAsync();
+
+                return await memberById.LoadAsync(memberIds, cancellationToken);
             }
         }
     }
