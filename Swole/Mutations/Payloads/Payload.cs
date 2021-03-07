7b7b5a14517662883190829ad808142f31eccf9f
te25son <te25son@gmail.com>
2021-03-07 10:20:20 +0100

    Add user error and payload base classes

---
+++ Swole/Mutations/Payloads/Payload.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Swole.Mutations.Payloads
+{
+    public abstract class Payload
+    {
+        protected Payload(IReadOnlyList<UserError>? errors) => Errors = errors;
+
+        public IReadOnlyList<UserError>? Errors { get; }
+    }
+}
